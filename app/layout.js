36255e473829da025d264618c68e2167b623b469
gbits021-dev <gbits021@gmail.com>
2025-10-07 16:52:56 +0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Design in Motion",
+  description: "DIO Architects Event 2025",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
